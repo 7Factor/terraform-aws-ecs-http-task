ff32a925ebf735f520ffd3eba600c0758692e484
Jeremy Wood <jeremy.wood@7factor.io>
2022-04-11 21:09:08 -0400

    Change lb_security_groups variable usage.

--- lb.tf
+++ lb.tf
@@ -4,7 +4,7 @@ module "app_lb" {
   cluster_name             = var.cluster_name
   app_name                 = var.app_name
   target_group_arn         = aws_lb_target_group.lb_targets.arn
-  security_groups          = [var.lb_security_groups]
+  security_groups          = var.lb_security_groups
   subnets                  = var.lb_subnets
   internal                 = var.lb_internal
   idle_timeout             = var.lb_idle_timeout
