facce6e82bfba7edac04f936325ac06a071e0ae2
Paul Skersis <p.skersis@gmail.com>
2023-02-07 15:53:00 -0600

    Update formatting

--- ecs.tf
+++ ecs.tf
@@ -3,12 +3,12 @@ data "aws_ecs_cluster" "target_cluster" {
 }
 
 resource "aws_ecs_task_definition" "main_task" {
-  family                = "${var.app_name}-tsk"
+  family                   = "${var.app_name}-tsk"
   requires_compatibilities = [var.launch_type]
-  network_mode          = "bridge"
-  cpu                   = var.cpu
-  memory                = var.memory
-  container_definitions = var.container_definition
+  network_mode             = "bridge"
+  cpu                      = var.cpu
+  memory                   = var.memory
+  container_definitions    = var.container_definition
 
   task_role_arn = var.task_role_arn
 
